5efec9c6a619bed082579d13a054f692742024fb
João Graciotim <graciotim92@gmail.com>
2024-10-09 22:51:41 -0300

    feature: nova conexão com banco de dados SQLite e criação da tabela hospedes / nova query inserirHospede

--- src/BancoDeDadosHospedes.js
+++ src/BancoDeDadosHospedes.js
@@ -1,3 +1,4 @@
+import sqlite from 'sqlite3';
 /**
  * Classe que interage com o banco de dados para gerenciar hóspedes.
  * Os candidatos devem implementar a lógica de persistência de dados.
@@ -5,6 +6,26 @@
 class BancoDeDadosHospedes {
     constructor() {
         // TODO: Implementar o construtor para inicializar a conexão com o banco de dados (SQLite)
+        this.db = new sqlite.Database("bancodedados.db", async (err) => {
+            if (err) {
+                console.error("Erro ao conectar com o banco:", err.message)
+            } else {
+                console.log("Conectado com o banco")
+                await this.criarTabelas()
+            }
+        })
+    }
+
+    async criarTabelas() {
+        const criarTabelaHospedes = "CREATE TABLE IF NOT EXISTS hospedes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, quarto INTEGER NOT NULL)"
+
+        this.db.run(criarTabelaHospedes, (err) => {
+            if(err) {
+                console.error("Erro ao executar SQL:", err.message)
+            } else {
+                console.log("Tabela hospedes criada")
+            }
+        })
     }
 
     /**
@@ -16,6 +37,18 @@ class BancoDeDadosHospedes {
     inserirHospede(nome, quarto) {
         // TODO: Implementar a lógica para inserir um hóspede no banco de dados
         // Retornar uma Promise que resolve com o ID do hóspede inserido
+
+        return new Promise((resolve, reject) => {
+            const queryInsereHospede = "INSERT INTO hospedes (nome, quarto) VALUES (?, ?)"
+
+            this.db.run(queryInsereHospede, [nome, quarto], function(err) {
+                if(err) {
+                    reject(err)
+                } else {
+                    resolve(this.lastID)
+                }
+            })
+        })
     }
 
     /**
@@ -39,5 +72,15 @@ class BancoDeDadosHospedes {
         // Retornar uma Promise que resolve quando a operação estiver completa
     }
 }
+
+const bancoDeDados = new BancoDeDadosHospedes()
+
+try {
+    const idHospede = await bancoDeDados.inserirHospede("João Graciotim", 22)
+    console.log(idHospede)
+} catch (err) {
+    console.error("Erro ao inserir hospede:", err.message)
+}
+
 
 export default BancoDeDadosHospedes
